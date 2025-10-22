39e44bab77dff71c82b3a164172defe69582d97c
smashpop7 <officl.ahmed@gmail.com>
2025-10-22 23:41:24 +0600

    Update models.py

--- sofor_sathi/tripapp/models.py
+++ sofor_sathi/tripapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from userapp.models import UserProfile
 
-# Create your models here.
+
+class BucketList(models.Model):
+
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    
+
+class Trip(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+   
+
+
+class Budget(models.Model):
+
+    trip = models.OneToOneField(Trip, on_delete=models.CASCADE)
+    transport_cost = models.IntegerField(default=0)
+    food_cost = models.IntegerField(default=0)
+    hotel_cost = models.IntegerField(default=0)
+    other_cost = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"budget for {self.trip}"
