cc463ba9ce0d7490b591497612a8a54f46c43664
smashpop7 <officl.ahmed@gmail.com>
2025-10-22 23:43:30 +0600

    Update admin.py

--- sofor_sathi/tripapp/admin.py
+++ sofor_sathi/tripapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BucketList, Trip, Budget
 
-# Register your models here.
+admin.site.register(BucketList)
+admin.site.register(Trip)
+admin.site.register(Budget)
