cd622c1f99e28628b4c4f8738ea690d8f4806529
Riyad Hasan <riyadhasan7459@gmail.com>
2025-10-23 00:23:08 +0600

    Update admin.py

--- sofor_sathi/tourismapp/admin.py
+++ sofor_sathi/tourismapp/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Location, Route, LocationReview, RouteReview
+
+# register models
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Route)
+admin.site.register(LocationReview)
+admin.site.register(RouteReview)
