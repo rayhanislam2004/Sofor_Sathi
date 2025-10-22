5fb104cc0284948f74f9cb1bdf9ea7bf8e3247b7
smashpop7 <officl.ahmed@gmail.com>
2025-10-22 23:57:01 +0600

    Create urls.py

---
+++ sofor_sathi/tripapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'tripapp'
+
+urlpatterns = [
+
+    path('bucket/add/<int:location_id>/', views.add_to_bucket_list, name='add_to_bucket_list'),
+
+    path('bucket/', views.bucket_list_view, name='bucket_list'),
+]
