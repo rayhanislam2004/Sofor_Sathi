213f765ab8b38675c6e2fe7c0b5c2d5aa0fdbdc9
smashpop7 <officl.ahmed@gmail.com>
2025-10-23 10:09:17 +0600

    Update views.py

--- sofor_sathi/tripapp/views.py
+++ sofor_sathi/tripapp/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from tourismapp.models import Location
+from .models import BucketList, Trip, Budget
+from .forms import TripForm, BudgetForm
 
-# Create your views here.
+
+@login_required
+def add_to_bucket_list(request, location_id):
+    location = get_object_or_404(Location, id=location_id)
+
+    if not BucketList.objects.filter(user=request.user.userprofile, spot=location).exists():
+        BucketList.objects.create(user=request.user.userprofile, spot=location)
+        messages.success(request, f'"{location.name}" has been added to your bucket list!')
+    else:
+        messages.warning(request, f'"{location.name}" is already in your bucket list.')
+
+    return redirect('tourismapp:location_detail', pk=location_id)
+
+
+@login_required
+def plan_trip(request, location_id):
+    location = get_object_or_404(Location, id=location_id)
+
+
+    if request.method == 'POST':
+        trip_form = TripForm(request.POST)
+        budget_form = BudgetForm(request.POST)
+
+        if trip_form.is_valid() and budget_form.is_valid():
+
+            trip = trip_form.save(commit=False)
+
+            trip.user = request.user.userprofile
+            trip.spot = location
+            trip.save()
+            budget = budget_form.save(commit=False)
+            budget.trip = trip
+            budget.save()
+
+            messages.success(request, f"Your trip to {location.name} has been planned!")
+
+            return redirect('userapp:profile')
+
+    else:
+        trip_form = TripForm()
+        budget_form = BudgetForm()
+
+    context = {
+        'location': location,
+        'trip_form': trip_form,
+        'budget_form': budget_form
+    }
+    return render(request, 'tripapp/plan_trip.html', context)
+
+
+@login_required
+def bucket_list_view(request):
+    bucket_list_items = BucketList.objects.filter(user=request.user.userprofile)
+    context = {'bucket_list_items': bucket_list_items}
+    return render(request, 'tripapp/bucket_list.html', context)
