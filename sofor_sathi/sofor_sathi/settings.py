b7a2661da2155ae19172fc2bf4df24f26cf9b159
junaidalmahir25 <23101058@uap-bd.edu>
2025-10-23 19:26:20 +0600

    install contribution

--- sofor_sathi/sofor_sathi/settings.py
+++ sofor_sathi/sofor_sathi/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'userapp',
     'tripapp',
     'tourismapp',
+    'contribution',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
