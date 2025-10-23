eb0c83d16b8dafd0982f120985f4d98c17d01714
Riyad Hasan <riyadhasan7459@gmail.com>
2025-10-23 10:13:11 +0600

    install tourismapp.py

--- sofor_sathi/sofor_sathi/settings.py
+++ sofor_sathi/sofor_sathi/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'userapp',
     'tripapp',
+    'tourismapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
