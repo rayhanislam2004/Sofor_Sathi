529efd65727dba59b0711090890b408c776713c7
Rayhan Islam <123mdray@gmail.com>
2025-10-23 20:38:53 +0600

    admin interface for location and route models
    Added admin actions and custom admin classes for Location and Route models.

--- sofor_sathi/tourismapp/admin.py
+++ sofor_sathi/tourismapp/admin.py
@@ -4,7 +4,42 @@ from .models import Category, Location, Route, LocationReview, RouteReview
 
 # register models
 admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Route)
 admin.site.register(LocationReview)
 admin.site.register(RouteReview)
+
+
+@admin.action(description='Approve selected items')
+def approve_selected(modeladmin, request, queryset):
+    queryset.update(is_approved=True)
+
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'location',
+        'category',
+        'submitted_by',
+        'is_approved'  
+    )
+
+    list_filter = ('is_approved', 'category', 'submitted_by')
+    search_fields = ('name', 'location', 'description')
+
+    actions = [approve_selected]
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',  
+        'submitted_by',
+        'is_approved'
+    )
+    list_filter = ('is_approved', 'submitted_by', 'location')
+    search_fields = ('start_point', 'end_point', 'description')
+    actions = [approve_selected]
+
+
