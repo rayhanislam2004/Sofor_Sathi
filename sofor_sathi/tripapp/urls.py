410345c838df584e428c19d5a4a136d8fea4587a
smashpop7 <officl.ahmed@gmail.com>
2025-10-23 10:08:31 +0600

    Update urls.py

--- sofor_sathi/tripapp/urls.py
+++ sofor_sathi/tripapp/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('bucket/add/<int:location_id>/', views.add_to_bucket_list, name='add_to_bucket_list'),
 
     path('bucket/', views.bucket_list_view, name='bucket_list'),
+
+    path('plan/<int:location_id>/', views.plan_trip, name='plan_trip'),
 ]
