3282bdc22900deae2dc07813f87a302b471888c2
smashpop7 <officl.ahmed@gmail.com>
2025-10-23 10:06:17 +0600

    correction  models.py

--- sofor_sathi/tripapp/models.py
+++ sofor_sathi/tripapp/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 from userapp.models import UserProfile
+from tourismapp.models import Location
 
 
 class BucketList(models.Model):
 
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    spot = models.ForeignKey(Location, on_delete=models.CASCADE)
     added_on = models.DateTimeField(auto_now_add=True)
 
-    
+    def __str__(self):
+        return f"{self.spot.name} in {self.user.user.username} bucket List"
+
 
 class Trip(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    spot = models.ForeignKey(Location, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
 
-   
+    def __str__(self):
+        return f"Trip to {self.spot.name} by {self.user.user.username}"
 
 
 class Budget(models.Model):
